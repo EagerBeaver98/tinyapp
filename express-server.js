30ca4358f8c6bfca823a61a0f19c6394c69485ef
EagerBeaver98 <jooke98@outlook.com>
2020-11-10 02:47:42 +0000

    generates random string for urls

--- express-server.js
+++ express-server.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+const generateRandomString = () => {
+  return Math.random().toString(36).substr(2, 6);
+};
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 app.set("view engine", "ejs");
 
@@ -16,6 +28,10 @@ app.get("/", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
