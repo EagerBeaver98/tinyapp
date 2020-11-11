a433259fb204e086c2944753068b21c61583eb1f
EagerBeaver98 <jooke98@outlook.com>
2020-11-11 05:03:41 +0000

    can log in and out. will remember user name

--- express-server.js
+++ express-server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 
 const generateRandomString = () => {
   return Math.random().toString(36).substr(2, 6);
@@ -30,17 +32,21 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[shortURL] };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[shortURL], username: req.cookies["username"] };
   res.render("urls_show", templateVars);
 });
 
@@ -63,10 +69,23 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect("/urls");
 });
 
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body.username);
+  console.log(`new user ${req.body.username}`);
+  res.redirect('/urls');
+});
+
 app.post("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL.replace(":");
+  console.log(`${urlDatabase[shortURL]} replaced by:`);
   urlDatabase[shortURL] = req.body.url;
+  console.log(urlDatabase[shortURL]);
   res.redirect(`/urls`);
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect('/urls');
 });
 
 app.listen(PORT, () => {
